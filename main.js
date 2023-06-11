ad88fa037f1d4169ce246a0a5e361475a5b93218
kaciczak0211 <kaciczak0211@gmail.com>
2023-06-11 10:30:41 +0100

    local_storage

--- main.js
+++ main.js
@@ -5,19 +5,7 @@ let bookForm = document.querySelector('#new-book-form');
 
 bookAdd.addEventListener("click", function(){
     bookForm.style.display = 'block';
-    let newDiv = document.createElement("div");
-    newDiv.setAttribute('id', "library");
-
 });
-
-function generator() {
-    random = Math.floor(Math.random() * 1000);
-    console.log(random);
-    return random
-}
-
-
-
 
 // new values 
 let libraryBank = document.querySelector("#library");
@@ -29,11 +17,18 @@ document.querySelector("#submit").addEventListener('click', function(){
 });
 document.querySelector("#close-btn").addEventListener('click', function(){
     bookForm.style.display = "none";
+
 });
 
-let myLibrary = []
+let myLibrary = [];
 
 
+
+function removeBook(i){
+    myLibrary.splice(i, 1);
+    render();
+    setData();
+}
 
 
 
@@ -48,48 +43,26 @@ function book(title, author, pages, read) {
 // render books from forms
 
 function render() {
-
     let libraryBank = document.querySelector("#library");
     libraryBank.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++){
         let singleBook = myLibrary[i];
 
         let singleBookEl = document.createElement("div");
-        singleBookEl.setAttribute('id', random + document.querySelectorAll("div").length);
-        singleBookEl.innerHTML = `<p>Title: ${singleBook.title} </p>`
+        singleBookEl.setAttribute("class", "book-section");
+        singleBookEl.innerHTML = `
+        <div class="header">
+        <h3>Title:${singleBook.title}</h3>
+        <h4>By:${singleBook.author}</h3>
+        </div>
+        <div class="addition">
+        <h4>Pages:${singleBook.pages}</h4>
+        <h4>Done:${singleBook.read}</h4>        
+        </div>
+        <button id="remove_btn" onclick="removeBook(${i})" >Remove</button>
+        `
         libraryBank.appendChild(singleBookEl);
-
-        let authorBookEl = document.createElement("div");
-        authorBookEl.setAttribute('id', random + document.querySelectorAll("div").length);
-        authorBookEl.innerHTML = `<p>Author: ${singleBook.author} </p>`
-        libraryBank.appendChild(authorBookEl);
-
-        let pagesBookEl = document.createElement("div");
-        pagesBookEl.setAttribute('id', random + document.querySelectorAll("div").length);
-        pagesBookEl.innerHTML = `<p>Pages: ${singleBook.pages} </p>`
-        libraryBank.appendChild(pagesBookEl);
-
-        let readBookEl = document.createElement("div");
-        readBookEl.setAttribute('id', random + document.querySelectorAll("div").length);
-        readBookEl.innerHTML = `<p>Read: ${singleBook.read} </p>`
-        libraryBank.appendChild(readBookEl);
-
-        
-        let removeBtn = document.createElement("div");
-        removeBtn.setAttribute('id', random);
-        removeBtn.innerHTML = `<button id="remove_btn">Remove</button>`
-        libraryBank.appendChild(removeBtn);
-
-        const removeButton = document.getElementById("remove_btn");
-
-        removeButton.addEventListener('click', function(){
-            singleBookEl.remove();
-            authorBookEl.remove();
-            pagesBookEl.remove();
-            readBookEl.remove();
-            removeBtn.remove();
-
-        });
+        console.log(singleBookEl);
     }
 
 }
@@ -103,13 +76,16 @@ function bookLibrary(){
     let newBook = new book(title, author, pages, read);
     myLibrary.push(newBook);
     render();
-
+    setData();
+    
 }
+
+
+
 
    document.querySelector('#new-book-form').addEventListener("submit", function(event){
     event.preventDefault();
     bookLibrary();
-
 }) 
 
 document.querySelector('#close-btn').addEventListener("click", function(event){
@@ -117,8 +93,20 @@ document.querySelector('#close-btn').addEventListener("click", function(event){
 
 }) 
 
-// const removeButton = document.getElementById("remove_btn");
-// removeButton.addEventListener('click', function(){
-//     let randomDiv = Math.floor(Math.random() * 100);
-//     console.log(randomDiv);
-// })
+
+function setData() {
+    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+};
+
+function restore() {
+    if(!localStorage.myLibrary) {
+        render();
+    }else {
+        let objects = localStorage.getItem('myLibrary')
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+        render();
+    }
+};
+
+restore();
